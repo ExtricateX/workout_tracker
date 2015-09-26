4eb1adc8c4863c4abc4829f453bacc841b6603e0
good4m <jeff86@gmail.com>
2015-09-26 09:50:52 -0700

    Signed-off-by: good4m <jeff86@gmail.com>

---
+++ WorkoutTacker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkoutTacker.MvcApplication" Language="C#" %>
---
+++ WorkoutTacker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkoutTacker.Startup))]
+namespace WorkoutTacker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
