d6925c8fad3ff709563559cf9ef53722e38113be
good4m <jeff86@gmail.com>
2015-10-12 01:30:42 -0700

    Replaced style with better one. Styled login page

--- WorkoutTracker/App_Start/BundleConfig.cs
+++ WorkoutTracker/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace WorkoutTracker
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,23 +13,15 @@ namespace WorkoutTracker
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/ripples.min.js",
-                "~/Scripts/material.min.js",
+                "~/Scripts/bootstrap.min.js",
                 "~/Scripts/respond.js"));
 
               bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css",
-                "~/Content/roboto.min.css",
-                "~/Content/material.min.css",
-                "~/Content/ripples.min.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/styles.css",
+                "~/Content/awesome-bootstrap-checkbox.css",
+                "~/Content/animate.css"));
         }
     }
 }
